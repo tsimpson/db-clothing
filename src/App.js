071f2372702cf41c3fd96ce957c6ad9d768991aa
tsimpson <travisrsimpson92@gmail.com>
2021-05-14 12:52:15 -0500

    added ability to store authenticated users to firebase db for signup

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shoppage';
 import SignInSignUp from './pages/signin-signup/signin-signup';
 import Header from './components/header/header';
-import  { auth } from './firebase/firebase.utils';
+import  { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -17,14 +17,27 @@ class App extends React.Component {
 
     this.state = {
       currentUser: null
-    }
+    };
   }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({  currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      } else {
+        this.setState({currentUser: userAuth});
+      }
     });
   }
 
--- src/pages/signin-signup/signin-signup.js
+++ src/pages/signin-signup/signin-signup.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in';
+import SignUp from '../../components/sign-up/sign-up';
 
 import './signin-signup.scss';
 
 const SignInSignUp = () => (
-  <div className='signin-signup'><SignIn /></div>
+  <div className='signin-signup'>
+    <SignIn />
+    <SignUp />
+  </div>
 );
 
 export default SignInSignUp;
